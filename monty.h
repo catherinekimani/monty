d3ba94b464896934b8dc3e2911cd2674f6569a40
catherinekimani <catherinekimani882@gmail.com>
2023-06-21 22:33:05 +0300

    Create structs

---
+++ monty.h
@@ -0,0 +1,55 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct pipe_s - structure to carry values
+ * @val: value
+ * @fd: file descriptor
+ * @ptr_content: content
+ *
+ */
+typedef struct pipe_s
+{
+	char *val;
+	FILE *fd;
+	char *ptr_content;
+} pipe_t;
+
+extern pipe_t global_var;
+
+#endif /*MONTY_H*/
