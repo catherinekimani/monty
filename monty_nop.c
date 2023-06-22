085b5fc17776986f15fd8cd539eaa989ae6c1727
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-06-22 14:03:10 +0000

    monty_nop.c

---
+++ monty_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *monty_nop- Does nothing
+  *@head: The head
+  *@count: Counter
+  *
+  */
+
+void monty_nop(stack_t **head, unsigned int count)
+{
+	(void)count;
+	(void)head;
+}
