ff8ab1f7774c7703de88e2eb72ca623d811f2dd0
catherinekimani <catherinekimani882@gmail.com>
2023-06-22 17:30:12 +0300

    Update header file with func prototypes

--- monty.h
+++ monty.h
@@ -3,9 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -37,19 +40,44 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct pipe_s - structure to carry values
+ * struct que_s - structure to carry values
  * @val: value
  * @fd: file descriptor
  * @ptr_content: content
- *
+ *@f: Integer value
  */
-typedef struct pipe_s
+typedef struct que_s
 {
 	char *val;
 	FILE *fd;
 	char *ptr_content;
-} pipe_t;
+	int f;
+} que_t;
 
-extern pipe_t global_var;
-
+extern que_t que;
+/*prototypes*/
+void monty_free_stack(stack_t *head);
+void monty_push(stack_t **head, unsigned int count);
+void monty_pall(stack_t **head, unsigned int count);
+void monty_addnode(stack_t **head, int n);
+void monty_pint(stack_t **head, unsigned int count);
+void monty_pop(stack_t **head, unsigned int count);
+void monty_swap(stack_t **head, unsigned int count);
+void monty_add(stack_t **head, unsigned int count);
+void monty_nop(stack_t **head, unsigned int count);
+void monty_div(stack_t **head, unsigned int count);
+void monty_sub(stack_t **head, unsigned int count);
+int execute_opcode(char *line_content,
+		   stack_t **stack, unsigned int line_number, FILE *fd);
+void monty_mul(stack_t **stack, unsigned int line_number);
+void monty_mod(stack_t **stack, unsigned int line_number);
+void monty_pchar(stack_t **stack, unsigned int line_number);
+void monty_pstr(stack_t **stack, unsigned int line_number);
+void monty_rotl(stack_t **stack,
+		__attribute__((unused)) unsigned int line_number);
+void monty_rotr(stack_t **stack,
+		__attribute__((unused)) unsigned int line_number);
+void monty_stack(stack_t **stack, unsigned int line_number);
+void monty_queue(stack_t **stack, unsigned int line_number);
+void monty_add_queue(stack_t **stack, int value);
 #endif /*MONTY_H*/
